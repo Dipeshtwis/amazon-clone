72b9f1a6dbee1d26253ec1e4b9de5b68cf3ae834
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-04-02 23:48:37 +0530

    Remove the unnecessary header option

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,10 +23,6 @@ function Header() {
                 <span className="header__optionLineOne">Your</span>
                 <span className="header__optionLineTwo">Prime</span>
               </div>
-              <div className="header__option">
-                <span className="header__optionLineOne">Hello Guest</span>
-                <span className="header__optionLineTwo">Sign In</span>
-              </div>
           </div>
         </div>
     )
