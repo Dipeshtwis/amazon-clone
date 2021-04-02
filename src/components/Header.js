946231a52de4dc5efcade9ec77e2954aa78b3963
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-04-03 00:54:57 +0530

    Style the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import '../assets/stylesheet/Header.css';
 
 function Header() {
@@ -22,6 +23,11 @@ function Header() {
               <div className="header__option">
                 <span className="header__optionLineOne">Your</span>
                 <span className="header__optionLineTwo">Prime</span>
+              </div>
+
+              <div className="header__optionBasket">
+                <ShoppingBasketIcon />
+                <span className="header__optionLineTwo header__basketCount">0</span>
               </div>
           </div>
         </div>
