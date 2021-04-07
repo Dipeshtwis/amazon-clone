d8a5565792aa745440bfb169c0176505bff66e83
Dipeshtwis <deepesh.97kumar@gmail.com>
2021-04-08 02:53:54 +0530

    Add react-router-dom and set path for checkout page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -5,10 +6,21 @@ import Home from './components/Home';
 function App() {
   return (
     //BEM
-    <div className="App">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>I AM IN CHECKOUT</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch> 
+      </div>
+    </Router>
+    
   );
 }
 
